5e5117e4231fc8ecbe4223b7b015ebd15b6751e3
txwdzxq <txwdzxq@sina.com>
2019-06-17 11:34:36 +0800

    2019年6月17日 11:34:33

--- src/main/java/com/eoner/generate/Generate.java
+++ src/main/java/com/eoner/generate/Generate.java
@@ -1,5 +1,17 @@
 package com.eoner.generate;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
 /**
  * @author mi
  * @date 2019/6/6 9:30
@@ -7,28 +19,27 @@ package com.eoner.generate;
 public class Generate {
     public static void main(String[] args) {
         // args = new String[]{"-configfile", "src\\main\\resources\\mybatis-generator.xml", "-overwrite"};
-    
-        // try {
-        //     List<String> warnings = new ArrayList<String>();
-        //     boolean overwrite = true;
-        //     ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        //     InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
-        //     ConfigurationParser cp = new ConfigurationParser(warnings);
-        //     Configuration config = cp.parseConfiguration(is);
-        //     DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-        //     MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-        //     myBatisGenerator.generate(null);
-        // } catch (SQLException e) {
-        //     e.printStackTrace();
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // } catch (InterruptedException e) {
-        //     e.printStackTrace();
-        // } catch (InvalidConfigurationException e) {
-        //     e.printStackTrace();
-        // } catch (XMLParserException e) {
-        //     e.printStackTrace();
-        // }
+        try {
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
         String resultMap = ResultMapUtil.getResultMap(InfectiousPatientInformationVO.class);
         System.out.println(resultMap);
     }
